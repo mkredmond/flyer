37f0b97d9d0cb77f35cdf43f6af9136751c59f5c
mkdeveloper56@gmail.com <mkdeveloper56@gmail.com>
2016-05-27 15:31:45 -0400

    Initial push to github

---
+++ resources/views/flyers/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')	
+
+@section('content')
+<h1>
+	Are you selling your house?
+</h1>
+<form action="{{ url('flyers') }}" method="POST" role="form" enctype="multipart/form-data">
+	@include('flyers.forms.form')
+</form>
+@stop
+
+
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Project Flyer</title>
+  <link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="{{ url('/') }}">ProjectFlyer</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+         
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+
+
+  <div class="container">
+    @yield('content')
+  </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
